109d025bed07398236b45c47b9061a65a326a721
amartins777 <amartins77@educarex.es>
2024-11-18 10:38:24 +0100

    Add files via upload

---
+++ ProcesosporLote/Prueba4.bat
@@ -0,0 +1,12 @@
+@echo off
+rem Script de Prueba
+rem Autor: Azara Martin
+rem Fecha: 18-11-2024
+
+set name=Azara
+set num1=6
+set num2=8
+set num3=4
+set /A suma=num1+num2+num3
+set /A media= suma/3
+echo La media de %sum% es %media%
