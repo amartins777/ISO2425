3cf4606a4a82bbb6215d07c62e31f24143093397
amartins777 <amartins77@educarex.es>
2025-02-18 18:41:34 +0100

    Calculadora.sh

--- Shell Scripts/Calculadora.sh
+++ Shell Scripts/Calculadora.sh
@@ -5,9 +5,11 @@
 
 clear
 
+FICHERO=$1
+OPERACIONES=$2
 echo "Autor: Azara Martín Sánchez"
 
-if [ $1 -ne 2 ]; then 
+if [ $# -ne 2 ]; then 
 
 	echo "Error, parámetro incorrecto "
 	exit
@@ -23,4 +25,35 @@ read -p "Operación Op1 Op2 " oper op1 op2
 
 num=0
 
-while [ 
+while [ $num -lt $2 ]; do
+
+    if [ $oper == "X" ]; then
+        break
+    fi
+    
+    case $oper in
+        S) 
+		RES=$((op1 + op2)) 
+	;;
+        R) 
+		RES=$((op1 - op2)) 
+	;;
+        M) 
+		RES=$((op1 * op2)) 
+	;;
+        D) 
+		RES=$((op1 / op2))
+        ;;
+	
+        *) 
+		echo "Operación no válida."
+	;;
+   
+    esac
+
+	echo "Resultado: $RES"
+	echo "$oper $op1 $op2" >> $1
+
+done
+echo "Fichero guardado en: $(pwd)/$1"
+cat $1    
