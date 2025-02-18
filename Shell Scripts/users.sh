e8fb69b664aad597f255f5eff4fcc94b8017f137
amartins777 <amartins77@educarex.es>
2025-02-18 08:37:40 +0100

    Add files via upload

---
+++ Shell Scripts/users.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script Entregable4 Users
+# Autor: Azara Martín Sánchez
+# Fecha: 11-02-2025
+
+clear
+
+IFS=:
+fecha=$(date +%d-%m-%Y)
+hora=$(date +%H:%M)
+echo "Informe de usuarios el dia $fecha a las $hora"
+
+num=1000
+
+while read us x uid gid d h shell; do
+
+	if [ $uid -ge 1000 ]; then
+		echo "$us - $uid "
+
+
+
+	fi
+
+	count=$(echo $uid | wc -l)
+done < /etc/passwd
+
+echo "$fecha $hora El usuario $us ha solicitado un informe de ususarios " >> /tmp/logeventos
